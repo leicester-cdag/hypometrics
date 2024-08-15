d6ddc919afa30e337649197893687e7af0df0df3
GilKCL <gilberte.martine_Edith@kcl.ac.uk>
2024-08-15 08:48:07 +0100

    More changes to package and github pages and added CGM functions with error messages to address #20

--- R/cgmInterpolate.R
+++ R/cgmInterpolate.R
@@ -1,103 +1,160 @@
-#' @title cgmInterpolate
+#' @title Implicit to Explicit Missingness and Linear Interpolation of Glucose Data.
 #'
-#' @description Interpolate CGM values down to one minute epochs.
+#' @description Transforms implicit gaps in CGM timestamps into explicit
+#' missing glucose values, and linearly interpolate glucose values if needed.
 #'
-#' @param DataFrame A dataframe of CGM data which will be interpolated to one minute epochs.
-#' Must contain columns: id, time, gl.
+#' @param DataFrame A dataframe of CGM data which will be filled and interpolated
+#' where specified. Must contain columns: id, cgm_timestamp and glucose.
+#' @param Interpolate Logical value (TRUE/FALSE) which determines whether
+#' linear interpolation should be conducted. If FALSE, implicit gaps in CGM timestamps
+#' will be turned into explicit gaps and no further changes to the data will be made.D
+#' Default is TRUE so that linear interpolation is conducted.
+#' @param MinGap Numeric value which defines the minimum duration in seconds
+#' of gaps to be filled and interpolated. Default is 120 seconds, i.e. 2 minutes
+#' @param MaxGap Numeric value which defines the maximum duration in seconds
+#' of gaps to be filled and interpolated. Default is 1800 seconds, i.e. 30 mines
+#' @param Granularity Numeric value which defines the granularity of the
+#' Glucose data desired in seconds. Typically closely linked to the MinGap parameter e.g.
+#' if granularity of data needed is 60 seconds, MinGap would be 120 so that gap is
+#' wide enough to add extra CGM timestamps. Default is 60 seconds or 1 min. i.e. the
+#' resulting data frame will have CGM data at the 1 -min level.
 #'
-#' @return A dataframe of CGM data interpolated to 1 minute epochs.
-#' @export
+#' @details
+#' This functions firstly turns implicit gaps in CGM timestamps into explicit
+#' missing glucose values. Secondly, if needed, it interpolates missing glucose value as defined
+#' by the MinGap, MaxGap and Granularity parameters.
+#'
+#' @return A dataframe of CGM data interpolated. Gaps above the maximum duration
+#' are left as is.
 #'
 #' @examples
 #' \dontrun{
 #' hypometrics::cgmInterpolate(DataFrame)
 #' }
-cgmInterpolate = function(DataFrame){
+#'
+#' @export
+cgmInterpolate <- function(DataFrame,
+                          Interpolate = TRUE,
+                          MinGap = 120,
+                          MaxGap = 1800,
+                          Granularity = 60){
 
-  cgmtimestamps <- c(DataFrame$time)
-  diff_time_sec <- difftime(DataFrame$time, dplyr::lag(DataFrame$time), units="secs")
+  #### Check function arguments and data frame columns ####
+
+  chk::check_names(DataFrame, names = c("id", "cgm_timestamp", "glucose"))
+
+  chk::chk_character(DataFrame$id)
+
+  checkmate::assertPOSIXct(DataFrame$cgm_timestamp)
+
+  chk::chk_numeric(DataFrame$glucose)
+
+  chk::chk_flag(Interpolate)
+
+  chk::chk_numeric(MinGap)
+
+  chk::chk_numeric(MaxGap)
+
+  chk::chk_numeric(Granularity)
+
+  #### Function within the function which will turn implicit glucose missingness into explicit and fill gaps where specified for each id ####
+  fill <- function(df){
+
+  cgmtimestamps <- c(df$cgm_timestamp)
+  diff_time_sec <- difftime(df$cgm_timestamp, dplyr::lag(df$cgm_timestamp), units="secs")
   diff_time_sec <- diff_time_sec[!is.na(diff_time_sec)]
   timediff_timestamps <- as.numeric(diff_time_sec)
 
-  ####Below is the 5-min interpolation but had to change this to 1-min so that Zeinab doesnt have to do that retrospectively
-  # indices_of_interest <- which(timediff_timestamps>=600 & timediff_timestamps<=1800)
-  #
-  # new_timestamps_smallgaps <- sapply(indices_of_interest, function (x)
-  #   if (timediff_timestamps[x] %% 300 !=0){
-  #     seq(cgmtimestamps[x]+300, cgmtimestamps[x+1]-(timediff_timestamps[x] %% 300), 300)
-  #   }else{
-  #     seq(cgmtimestamps[x]+300, cgmtimestamps[x+1]-300, 300)
-  #   })
+  #Identify indices of timestamps where there is a sufficient gap (between min and max) to add missing cgm timestamps
+  indices_of_interest <- which(timediff_timestamps>=MinGap & timediff_timestamps<=MaxGap)
 
-  indices_of_interest <- which(timediff_timestamps>=120 & timediff_timestamps<=1800) #aim being interpolating gaps of minimum 2 mins (so you can fit in a timestamp in between) and maximum of 30mins
-
+  #Add cgm timestamps in between min and max gap up at the Granularity level decided until there is not sufficient space in the time gap
   new_timestamps_smallgaps <- sapply(indices_of_interest, function (x)
-    if (timediff_timestamps[x] %% 60 !=0){
-      seq(cgmtimestamps[x]+60, cgmtimestamps[x+1]-(timediff_timestamps[x] %% 60), 60)#this will create timestamps in between the original timestamps up until it can't fit any more timestamps - note if I don't want to do that (check with Zeinab) I can fill it up until  cgmtimestamps[x+1]-((timediff_timestamps[x] %% 60)+60) and that would avoid very close in time timestamps
+    if (timediff_timestamps[x] %% Granularity !=0){
+      seq(cgmtimestamps[x]+ Granularity, cgmtimestamps[x+1]-(timediff_timestamps[x] %% Granularity), Granularity)
     }else{
-      seq(cgmtimestamps[x]+60, cgmtimestamps[x+1]-60, 60)
+      seq(cgmtimestamps[x]+ Granularity, cgmtimestamps[x+1]- Granularity, Granularity)
     })
 
 
   timestamps_for_smallgaps <- c(unlist(new_timestamps_smallgaps))
   timestamps_for_smallgaps <- data.frame(timestamps_for_smallgaps)
 
-  timestamps_for_smallgaps <- timestamps_for_smallgaps |>
-    dplyr::rename(CGM_Timestamp = timestamps_for_smallgaps) |>
-    transform(CGM_Timestamp = lubridate::as_datetime(CGM_Timestamp)) |>
-    dplyr::arrange(CGM_Timestamp)
+  timestamps_for_smallgaps <- timestamps_for_smallgaps %>%
+    dplyr::rename(cgm_timestamp = timestamps_for_smallgaps) %>%
+    transform(cgm_timestamp = lubridate::as_datetime(cgm_timestamp)) %>%
+    dplyr::arrange(cgm_timestamp)
 
-  DataFrame <- dplyr::full_join(DataFrame, timestamps_for_smallgaps, by = c("time" = "CGM_Timestamp"))
-  DataFrame<- dplyr::arrange(DataFrame, time)
+  df <- dplyr::full_join(df,
+                         timestamps_for_smallgaps,
+                         by = c("cgm_timestamp" = "cgm_timestamp"))
+  df <- dplyr::arrange(df, cgm_timestamp)
 
-  DataFrame$gl <-
-    approx(DataFrame$time, DataFrame$gl, DataFrame$time, method = "linear")$y
+  if(Interpolate == TRUE){
+#If interpolation of glucose values is required, a linear interpolation will be conducted.
+  df$glucose <-
+    stats::approx(df$cgm_timestamp, df$glucose, df$cgm_timestamp, method = "linear")$y
 
-  indices_for_largegaps <- which(timediff_timestamps>1800)
+  }
 
-  if(pracma::isempty(indices_for_largegaps)){#i.e. if participant has no gaps that are larger than 30 mins than do not create further explicit gaps in their data e.g. 07073 who has only 1 day worth of 15-min data
-    print("CGM data has no gaps larger than 30min")
+  indices_for_largegaps <- which(timediff_timestamps>MaxGap)
+ #i.e. if participant has no gaps that are larger than MaxGaps than will not create further explicit gaps in their data
+  if(pracma::isempty(indices_for_largegaps)){
 
-    DataFrame <- DataFrame |>
-      dplyr::arrange(time) |>
-      dplyr::mutate_if(is.numeric, round, digits = 2) |>
+    df <- df %>%
+      dplyr::arrange(cgm_timestamp) %>%
+      dplyr::mutate_if(is.numeric, round, digits = 2) %>%
       dplyr::mutate(
-        id =  DataFrame$id[1],
-        time = lubridate::floor_date(time, unit = "minutes")
+        id =  df$id[1],
+        cgm_timestamp = lubridate::floor_date(cgm_timestamp, unit = "minutes")
       )
 
-  } else{
-    print("CGM data has gaps larger than  30min ")
+  } #i.e. if participant has gaps longer than MaxGap, the extra timestamps will be created and glucose values marked as NA
+  else{
+
 
 
     new_timestamps_largegaps <- sapply(indices_for_largegaps, function (x)
-      if (timediff_timestamps[x] %% 60 !=0){
-        seq(cgmtimestamps[x]+60, cgmtimestamps[x+1]-(timediff_timestamps[x] %% 60), 60)
+      if (timediff_timestamps[x] %% Granularity !=0){
+        seq(cgmtimestamps[x]+Granularity, cgmtimestamps[x+1]-(timediff_timestamps[x] %% Granularity), Granularity)
       }else{
-        seq(cgmtimestamps[x]+60, cgmtimestamps[x+1]-60, 60)
+        seq(cgmtimestamps[x]+Granularity, cgmtimestamps[x+1]-Granularity, Granularity)
       })
 
     timestamps_for_largegaps <- c(unlist(new_timestamps_largegaps))
     timestamps_for_largegaps <- data.frame(timestamps_for_largegaps)
 
-    timestamps_for_largegaps <- timestamps_for_largegaps |>
-      dplyr::rename(time = timestamps_for_largegaps) |>
-      transform(time = lubridate::as_datetime(time)) |>
-      dplyr::arrange(time)
+    timestamps_for_largegaps <- timestamps_for_largegaps %>%
+      dplyr::rename(cgm_timestamp = timestamps_for_largegaps) %>%
+      transform(cgm_timestamp = lubridate::as_datetime(cgm_timestamp)) %>%
+      dplyr::arrange(cgm_timestamp)
 
-    DataFrame <- dplyr::full_join(DataFrame, timestamps_for_largegaps, by = "time")
-    print("Got to the end of creating explicits gaps in CGM data for when gap is longer than 30 mins")
+    df <- dplyr::full_join(df, timestamps_for_largegaps, by = "cgm_timestamp")
 
-    DataFrame <- DataFrame |>
-      dplyr::arrange(time) |>
-      dplyr::mutate_if(is.numeric, round, digits = 2) |>
-      dplyr::mutate(
-        id = DataFrame$id[1],
-        time = lubridate::floor_date(time, unit = "minutes")
-      )
+    df <- df %>%
+      dplyr::arrange(cgm_timestamp) %>%
+      dplyr::mutate_if(is.numeric, round, digits = 2) %>%
+      dplyr::mutate(id = df$id[1],
+                    cgm_timestamp = lubridate::floor_date(cgm_timestamp, unit = "minutes"))
 
-    return(DataFrame)
+    return(df)
   }
+
+  }
+
+  #### Splitting the DataFrame by id ####
+  list_participants <- split(DataFrame, DataFrame$id)
+
+  #### Calling the function for each data frame containing individual participants data ####
+  list_data <- lapply(list_participants, fill)
+
+  #### Row binding individual participants data in a single dataframe ####
+  final_data <- dplyr::bind_rows(list_data, .id = "id")
+
+  #### Return CGM data with explicit gaps and interpolated glucose if specified ####
+  return(final_data = data.frame(final_data))
+
+
 }
 
 
